8df5fe5807007dbe7180e6943d8dab98ced6c3ba
Chelli-Satish <89716615+Chelli-Satish@users.noreply.github.com>
2021-09-08 15:39:45 +0530

    Add files via upload

---
+++ 03_Implementation/satishheader.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <ctype.h>
+/*----------------------------------------------------------------------------*/
+/* Define Functions*/
+void clearInput(void);
+void addNewcontact(void);
+void listAll(void);
+void deletecontact(void);
+void modifycontact(void);
+int findcontact(void);
+int prompt(void);
+int findnum (int);
